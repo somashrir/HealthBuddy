70363d4e055b175f519df3f7e12f733aaaa02cfa
someshwarjha1 <someshwarjha7@gmail.com>
2023-03-23 13:06:47 +0530

    Calorie page backend added

--- src/Components/calorie.js
+++ src/Components/calorie.js
@@ -1,82 +1,90 @@
-import React, { useState } from "react";
-import styles from "./CSS/calorie.module.css";
-import calorie from "../Assets/calorie.png";
+import React, { useState, useEffect } from 'react';
+import styles from './CSS/calorie.module.css';
+import calorie from '../Assets/calorie.png';
+import Navbar from './Navbar';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 function Calorie() {
-  const [todos, setTodos] = useState([]);
-  const [input, setInput] = useState("");
-  const [cal, setCal] = useState(10);
-  const [counter, setCounter] = useState(0);
+  const [user_id, setUser_id] = useState(null);
+  const [query, setQuery] = useState('');
+  const [calories, setCalories] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInput(newValue);
-  }
+  useEffect(() => {
+    // Fetch the token from local storage
+    const token = localStorage.getItem('token');
 
-  function addItem() {
-    setTodos((prevTodos) => {
-      console.log(input);
-      return [...prevTodos, input];
-    });
-    setInput("");
-    console.log(todos);
-  }
+    if (token) {
+      // Decode the token to get the user_id
+      const decodedToken = jwt_decode(token);
+      const user_id = decodedToken.user_id;
 
-  function removeItem(id) {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((item, index) => {
-        return index !== id;
-      });
-    });
-  }
+      // Set the user_id in state
+      setUser_id(user_id);
+    }
+  }, []);
 
-  function handleCount() {
-    setCounter(cal + counter);
-  }
-
+  const handleClick = () => {
+    // Make the API call with the user_id in headers
+    axios
+      .post(
+        '/api/v1/calories',
+        {
+          query: query,
+          user_id: user_id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data)
+        setCalories(response.data.foods);
+      })
+      .catch((error) => console.log(error));
+  };
   return (
-    <div className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.heading}>
-          <h1 className={styles.head}>What did you eat today?</h1>
+    <>
+      <Navbar />
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.heading}>
+            <h1 className={styles.head}>What did you eat today?</h1>
 
-          <img className={styles.img} src={calorie} />
-        </div>
-        <h1>Total Calories={counter} cal </h1>
-        <div className={styles.form}>
-          <input
-            className={styles.input}
-            type="text"
-            value={input}
-            placeholder="Enter Food"
-            onChange={handleChange}
-          />
-          <button
-            className={styles.button}
-            onClick={() => {
-              addItem();
-              handleCount();
-            }}
-          >
-            <span className={styles.search}>Search</span>
-          </button>
-        </div>
-        <div>
-          <ul>
-            {todos.map((todoItem, index) => {
-              return (
-                <div id={index}>
-                  <li className={styles.listitem}>
-                    <span>{todoItem}</span>
-                    <span>{cal} cal</span>
-                  </li>
-                </div>
-              );
-            })}
-          </ul>
+            <img className={styles.img} src={calorie} />
+          </div>
+          <h1>Total Calories= cal </h1>
+          <div className={styles.form}>
+            <input
+              className={styles.input}
+              type="text"
+              placeholder="Enter Food"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <button className={styles.button} onClick={handleClick}>7
+              <span className={styles.search}>Search</span>
+            </button>
+          </div>
+          <div>
+            <ul>
+              {calories.map((food) => {
+                return (
+                  <div>
+                    <li className={styles.listitem}>
+                      <span>{food.name}</span>
+                      <span>{food.calorie} cal</span>
+                    </li>
+                  </div>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
