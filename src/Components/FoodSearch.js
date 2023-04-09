ec5c75c02d7f021c4ed0be277a368ab5d3894b6f
somashrir <somashri.rastogi@byjus.com>
2023-04-09 22:56:56 +0530

    --

--- src/Components/FoodSearch.js
+++ src/Components/FoodSearch.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
-import { Pie } from "react-chartjs-2";
-import Navbar from "./Navbar";
-import styles1 from "./CSS/medicine.module.css";
+import React, { useState, useEffect } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import Navbar from './Navbar';
+import axios from 'axios';
+import styles1 from './CSS/medicine.module.css';
 
 ChartJS.register(ArcElement, Tooltip);
 
 export function FoodSearch({
+  Calorie,
   Fat,
   Sodium,
   Carbs,
@@ -17,24 +19,49 @@ export function FoodSearch({
   color3,
   color4,
   color5,
+  color6,
 }) {
+  const [query, setQuery] = useState('');
+  const [calorie, setCalorie] = useState(0);
+  const [fat, setFat] = useState(0);
+  const [sodium, setSodium] = useState(0);
+  const [carbs, setCarbs] = useState(0);
+  const [protein, setProtein] = useState(0);
+  const [sugar, setSugar] = useState(0);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post('/api/v1/foodSearch', { query: query })
+      .then((response) => {
+        setCalorie(response.data.calorie);
+        setFat(response.data.fat);
+        setSodium(response.data.sodium);
+        setCarbs(response.data.carbs);
+        setProtein(response.data.protein);
+        setSugar(response.data.sugar);
+        // redirect to home page or dashboard
+      })
+      .catch((error) => console.log(error));
+  };
+
   let data = {
-    labels: ["Fat", "Sugar", "Carbs", "Protein", "Sodium"],
+    labels: ["Calorie", "Fat", "Sugar", "Carbs", "Protein", "Sodium"],
     datasets: [
       {
         data: [
-          Fat || 120,
-          Sugar || 200,
-          Carbs || 100,
-          Protein || 30,
-          Sodium || 90,
+          Calorie || calorie,
+          Fat || fat,
+          Sugar || sugar,
+          Carbs || carbs,
+          Protein || protein,
+          Sodium || sodium,
         ],
         backgroundColor: [
           color1 || "#ffbe0b",
           color2 || "#fb5607",
           color3 || "#ff006e",
           color4 || "#3a86ff",
-          color5 || "#8338ec"
+          color5 || "#8338ec",
         ],
 
         borderWidth: 1,
@@ -65,7 +92,7 @@ export function FoodSearch({
       </div>
 
       <div className={styles1.piechart}>
-        <Pie data={data} height="50px" width="50px"/>
+        <Pie data={data} height="50px" width="50px" />
       </div>
     </>
   );
